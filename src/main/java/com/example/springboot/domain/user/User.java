893f803066f552843bbd6a9c6cd8e5c905b370c7
PhilSoGooood <peelhw@gmail.com>
2022-04-27 02:04:09 +0900

    Chapter 5.3 구글 로그인 연동하기

--- src/main/java/com/example/springboot/domain/user/User.java
+++ src/main/java/com/example/springboot/domain/user/User.java
@@ -6,8 +6,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 import com.example.springboot.domain.BaseTimeEntity;
 
